e6cb305f38702be8c8c18fd83a1938e713847638
NBM Project <anakusahamedia@gmail.com>
2026-07-17 06:22:58 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const input = document.getElementById("searchInput");
 const hero = document.getElementById("heroNews");
-function cariBerita(){
-const keyword = input.value.toLowerCase();
-const isi = hero.innerText.toLowerCase();
-if(isi.includes(keyword)){
-hero.style.border="3px solid red";
-hero.scrollIntoView({
-behavior:"smooth",
-block:"center"
-});
-}else{
-hero.style.border="none";
-}
-}
-input.addEventListener("keyup",cariBerita);
-document
-.getElementById("searchBtn")
-.addEventListener("click",cariBerita);
+const searchBtn = document.getElementById("searchBtn");
+
+if (input && hero && searchBtn) {
+
+    function cariBerita() {
+        const keyword = input.value.toLowerCase();
+        const isi = hero.innerText.toLowerCase();
+
+        if (isi.includes(keyword)) {
+            hero.style.border = "3px solid red";
+            hero.scrollIntoView({
+                behavior: "smooth",
+                block: "center"
+            });
+        } else {
+            hero.style.border = "none";
+        }
+    }
+
+    input.addEventListener("keyup", cariBerita);
+    searchBtn.addEventListener("click", cariBerita);
+
+                }
+
 
 const pageUrl = encodeURIComponent(window.location.href);
 const pageTitle = encodeURIComponent(document.title);
