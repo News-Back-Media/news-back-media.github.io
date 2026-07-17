f5eb0a40d2679efd8e2ce4336768a8c498d1220a
NBM Project <anakusahamedia@gmail.com>
2026-07-17 11:06:51 +0700

    Update article.js

--- article.js
+++ article.js
@@ -44,3 +44,22 @@ copy.textContent="Salin Link Berita";
 },2000);
 };
 }
+
+///
+const topBtn=document.getElementById("topBtn");
+window.addEventListener("scroll",()=>{
+
+if(window.scrollY>300){
+topBtn.style.display="block";
+}else{
+topBtn.style.display="none";
+}
+});
+
+topBtn.onclick=()=>{
+
+window.scrollTo({
+top:0,
+behavior:"smooth"
+});
+};
