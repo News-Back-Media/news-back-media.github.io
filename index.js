418b0271e0ec6184ad19d3d219c85eee800dc0d3
NBM Project <anakusahamedia@gmail.com>
2026-07-19 10:07:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -21,3 +21,57 @@ setTimeout(() => {
 document.getElementById("loader").classList.add("hide");
 }, 1500);
 });
+
+//
+const cards = document.querySelectorAll(".feed-card");
+
+const perPage = 5;
+let currentPage = 1;
+
+function showPage(page){
+
+currentPage = page;
+
+cards.forEach((card,index)=>{
+
+const start=(page-1)*perPage;
+const end=start+perPage;
+
+card.style.display =
+(index>=start && index<end)
+? "flex"
+: "none";
+
+});
+
+renderPagination();
+
+}
+
+function renderPagination(){
+
+const totalPages=Math.ceil(cards.length/perPage);
+
+const pagination=document.getElementById("pagination");
+
+pagination.innerHTML="";
+
+for(let i=1;i<=totalPages;i++){
+
+const btn=document.createElement("button");
+
+btn.textContent=i;
+
+if(i===currentPage){
+btn.classList.add("active");
+}
+
+btn.onclick=()=>showPage(i);
+
+pagination.appendChild(btn);
+
+}
+
+}
+
+showPage(1);
