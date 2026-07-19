2c9e94e55bb1a7c5b1f5037c75171d4f4694644c
NBM Project <anakusahamedia@gmail.com>
2026-07-19 10:34:56 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,101 +1,64 @@
 const input = document.getElementById("searchInput");
 const cards = document.querySelectorAll(".hero-card, .feed-card");
-
 function cariBerita() {
 const keyword = input.value.trim().toLowerCase();
-
 cards.forEach(card => {
 const isi = card.textContent.toLowerCase();
-
 if (keyword === "" || isi.includes(keyword)) {
 card.style.display = "";
 } else {
 card.style.display = "none";
 }
-
 });
 }
-
 if(input){
 input.addEventListener("input", cariBerita);
 }
-
 const searchBtn = document.getElementById("searchBtn");
 if(searchBtn){
 searchBtn.addEventListener("click", cariBerita);
 }
 
 //
-
 window.addEventListener("load", () => {
-
 setTimeout(() => {
-
 const loader = document.getElementById("loader");
-
 if(loader){
 loader.classList.add("hide");
 }
-
 },1500);
-
 });
 
 //
-
-// Pagination
 const feedCards = document.querySelectorAll(".feed-card");
 const pagination = document.getElementById("pagination");
-
 if(feedCards.length > 0 && pagination){
-
 const perPage = 5;
 let currentPage = 1;
-
 function showPage(page){
-
 currentPage = page;
-
 feedCards.forEach((card,index)=>{
-
 const start=(page-1)*perPage;
 const end=start+perPage;
-
 card.style.display =
 (index>=start && index<end)
 ? "flex"
 : "none";
-
 });
-
 renderPagination();
-
 }
-
 function renderPagination(){
-
 const totalPages=Math.ceil(feedCards.length/perPage);
-
 pagination.innerHTML="";
-
 for(let i=1;i<=totalPages;i++){
-
 const btn=document.createElement("button");
-
 btn.textContent=i;
-
 if(i===currentPage){
 btn.classList.add("active");
 }
-
 btn.onclick=()=>showPage(i);
-
 pagination.appendChild(btn);
-
 }
-
 }
-
 showPage(1);
-
 }
